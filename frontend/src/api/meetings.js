2f95985bd00f8577f9b2aad416a09af9e6abad7e
Anatolii Vasylov <anatolii.vasylov@student.tuke.sk>
2026-04-18 13:57:41 +0300

    add frontend api integration for meetings endpoints

---
+++ frontend/src/api/meetings.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000'
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+})
+
+export const submitMeeting = async ({ title, transcript }) => {
+  const { data } = await api.post('/meetings/text', { title, transcript })
+  return data
+}
+
+export const getAllMeetings = async () => {
+  const { data } = await api.get('/meetings')
+  return data
+}
+
+export const getMeetingById = async (id) => {
+  const { data } = await api.get(`/meetings/${id}`)
+  return data
+}
