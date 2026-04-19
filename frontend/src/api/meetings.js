cf670c19b5d2d97e76d7a1fe19c9bdfaf20192f8
Volodymyr Mishchuk <volodymyr.mishchuk@student.tuke.sk>
2026-04-19 15:00:02 +0200

    Update meetings.js

--- frontend/src/api/meetings.js
+++ frontend/src/api/meetings.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000'
+const BASE_URL = import.meta.env.VITE_API_URL
 
 const api = axios.create({
   baseURL: BASE_URL,
